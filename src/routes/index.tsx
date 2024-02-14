28c84fb3119a3305789c64abadb052277f0c3e2d
Caio Buarque de Gusmão Montenegro <caio_buarque@yahoo.com>
2024-02-14 15:05:00 -0300

    tela cadastro

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import Lgin from '../pages/Login';
+import Lgin from '../pages/Login/index';
+import Cadastro from '../pages/Cadastro/index'
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,11 @@ export default function Routes(){
             <Stack.Screen
             name='Login'
             component={Lgin}
+            options={{headerShown: false}}/>
+
+        <Stack.Screen
+            name='Cadastro'
+            component={Cadastro}
             options={{headerShown: false}}/>
         </Stack.Navigator>
     )
